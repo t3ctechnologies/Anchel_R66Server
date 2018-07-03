73fd89928d5817c0b500c469d5643e5a7c17ba73
Dayanand <dayanand.k@shrigowri.com>
2018-07-03 17:50:41 +0530

    first commit

---
+++ src/main/java/com/t3c/anchel/AnchelR66Server.java
@@ -0,0 +1,63 @@
+package com.t3c.anchel;
+
+import java.io.File;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletContextEvent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.ContextLoaderListener;
+import org.waarp.openr66.protocol.exception.OpenR66ProtocolPacketException;
+import org.waarp.openr66.server.R66Server;
+
+public class AnchelR66Server extends ContextLoaderListener{
+
+	private static final Logger logger = LoggerFactory.getLogger(AnchelR66Server.class);
+
+	public void contextInitialized(ServletContextEvent arg0) {
+
+		try {
+			new R66ServerDBInitializer().initdb();
+			logger.debug("Anchel R66Server is starting...");
+			File configFile = new File(this.getClass().getClassLoader().getResource("config-serverA.xml").getFile());
+			String path = null;
+			if (configFile.exists()) {
+				path = configFile.toString();
+			}
+			String[] waarpconfig = { path };
+			logger.debug("Anchel R66 server databse is initiating");
+			R66Server.initR66Server(waarpconfig);
+		} catch (OpenR66ProtocolPacketException e) {
+			e.printStackTrace();
+			logger.error("Anchel R66 server is not started " + e.getMessage());
+		} catch (SQLException e) {
+			e.printStackTrace();
+			logger.error("Anchel R66 database connection is not closed " + e.getMessage());
+		}
+	}
+
+	public void contextDestroyed(ServletContextEvent arg0) {
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+		while (drivers.hasMoreElements()) {
+			Driver driver = drivers.nextElement();
+			if (driver.getClass().getClassLoader() == cl) {
+				try {
+					logger.info("Deregistering JDBC driver {}", driver);
+					DriverManager.deregisterDriver(driver);
+				} catch (SQLException e) {
+					e.printStackTrace();
+					logger.error("Error deregistering JDBC driver {}", driver, e);
+				}
+			} else {
+				logger.trace("Not deregistering JDBC driver {} as it does not belong to this webapp's ClassLoader",
+						driver);
+			}
+		}
+		logger.error("Anchel R66 server is terminated");
+	}
+}
---
+++ src/main/java/com/t3c/anchel/R66ServerDBInitializer.java
@@ -0,0 +1,100 @@
+package com.t3c.anchel;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.waarp.openr66.server.ServerInitDatabase;
+
+public class R66ServerDBInitializer {
+
+	private static final Logger logger = LoggerFactory.getLogger(R66ServerDBInitializer.class);
+
+	public void initdb() throws SQLException {
+		Properties properties = new Properties();
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			properties.load(getClass().getClassLoader().getResourceAsStream("waarpdb.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String propcondition = properties.getProperty("com.sgs.waarpdb.auto");
+		String mycondition1 = new String("create");
+		String mycondition2 = new String("update");
+		File waarpconfigFile = new File(this.getClass().getClassLoader().getResource("config-serverA.xml").getFile());
+		File waarpauthFile = new File(this.getClass().getClassLoader().getResource("OpenR66-authent-A.xml").getFile());
+		File waarplimitFile = new File(this.getClass().getClassLoader().getResource("limitConfiga.xml").getFile());
+		String waarpconfig = waarpconfigFile.toString();
+		String directory = waarpconfig.substring(0, waarpconfig.lastIndexOf(File.separator));
+		String[] update = { waarpconfig, "-upgradeDb" };
+		if (propcondition.equals(mycondition1)) {
+			String[] waarpdbinit = { waarpconfig, "-initdb" };
+			String[] loadBusiness = { waarpconfig, "-loadBusiness", waarpconfig };
+			String[] loadAlias = { waarpconfig, "-loadAlias", waarpconfig };
+			String[] loadRoles = { waarpconfig, "-loadRoles", waarpconfig };
+			String[] loadRules = { waarpconfig, "-dir", directory };
+			String[] loadAuths = { waarpconfig, "-auth", waarpauthFile.toString() };
+			String[] loadLimit = { waarpconfig, "-limit", waarplimitFile.toString() };
+			
+			ServerInitDatabase.initR66database(waarpdbinit);
+			logger.debug("Anchel R66 server databse is initiated");
+
+			ServerInitDatabase.initR66database(loadBusiness);
+			logger.debug("Anchel R66 server, business configuration is loaded");
+			
+			ServerInitDatabase.initR66database(loadAlias);
+			logger.debug("Anchel R66 server, aliases configuration is loaded");
+			
+			ServerInitDatabase.initR66database(loadRoles);
+			logger.debug("Anchel R66 server, roles configuration is loaded");
+			
+			ServerInitDatabase.initR66database(loadRules);
+			logger.debug("Anchel R66 server, directory configuration is loaded");
+			
+			ServerInitDatabase.initR66database(loadAuths);
+			logger.debug("Anchel R66 server, auth configuration is loaded");
+			
+			ServerInitDatabase.initR66database(loadLimit);
+			logger.debug("Anchel R66 server, auth configuration is loaded");
+			
+			ServerInitDatabase.initR66database(update);
+
+			try {
+				String dbDriver = properties.getProperty("com.sgs.waarpdb.driver");
+				String dbServer = properties.getProperty("com.sgs.waarpdb.server");
+				String dbuser = properties.getProperty("com.sgs.waarpdb.user");
+				String dbpass = properties.getProperty("com.sgs.waarpdb.pass");
+
+				Class.forName(dbDriver);
+				conn = DriverManager.getConnection(dbServer, dbuser, dbpass);
+				stmt = conn.createStatement();
+
+				String s3mappingtab = "CREATE TABLE S3BUCKETMAPPING" + "(id int(11) NOT NULL AUTO_INCREMENT,"
+						+ "uuid varchar(450) NOT NULL," + "size varchar(450) NOT NULL,"
+						+ "s3fileurl varchar(450) NOT NULL," + "processedOn DATETIME," + "deleted char(1) NOT NULL,"
+						+ "PRIMARY KEY (id))";
+
+				stmt.executeUpdate(s3mappingtab);
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.error("Table S3BUCKETMAPPING is not created because of : " + e.getMessage());
+			} finally {
+				if (conn != null && stmt != null) {
+					conn.close();
+					stmt.close();
+				}
+			}
+		}
+		if (propcondition.equals(mycondition2)) {
+			ServerInitDatabase.initR66database(update);
+			logger.debug("Anchel R66 server, database is updated");
+		}
+	}
+}
